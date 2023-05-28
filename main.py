c2c8adb82a2f167f4bf03a00c85ab458f8358c75
xRetry <marcoschwarz.at@gmail.com>
2023-05-28 21:52:32 +0200

    Finish console output

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def create_trans_matrix_simple(dice_probs: np.ndarray) -> np.ndarray:
 
     return matrix
 
-def create_trans_matrix_complex(dice_probs: np.ndarray) -> np.ndarray:
+def create_trans_matrix_complex(dice_probs: np.ndarray, matrix_simple: np.ndarray) -> np.ndarray:
     matrix = np.zeros((SIZE+3, SIZE+3))
-    matrix[:SIZE, :SIZE] = create_trans_matrix_simple(dice_probs)
+    matrix[:SIZE, :SIZE] = matrix_simple
 
     # Go to start
     matrix[7, :] = np.zeros(SIZE+3)
@@ -40,7 +40,6 @@ def create_trans_matrix_complex(dice_probs: np.ndarray) -> np.ndarray:
 
     # Leave jail
     prob_equal = 1/NUM_DICE1 * 1/NUM_DICE2
-
     idxs_leave = 10+2*np.arange(1, 7)
     matrix[SIZE, idxs_leave] = prob_equal
     matrix[SIZE+1, idxs_leave] = prob_equal
@@ -58,7 +57,7 @@ def get_n_step_probs_power(matrix: np.ndarray, num_steps: int) -> np.ndarray:
 
     return np.linalg.matrix_power(matrix, num_steps).T @ init
 
-def get_n_step_probs(matrix: np.ndarray, num_steps: int) -> np.ndarray:
+def get_n_step_probs(matrix: np.ndarray, num_steps: float) -> np.ndarray:
     init = np.zeros(matrix.shape[1], dtype=float)
     init[0] = 1
 
@@ -67,10 +66,16 @@ def get_n_step_probs(matrix: np.ndarray, num_steps: int) -> np.ndarray:
     probs_stationary = eig_vecs_left[0] / eig_vecs_left[0].sum()
 
     if np.isinf(num_steps):
-        return np.real(probs_stationary @ matrix)
+        probs = np.real(probs_stationary @ matrix)
+    else:
+        matrix_n_steps = eig_vecs_right @ np.diag(eig_vals**num_steps) @ eig_vecs_left
+        probs = np.real(matrix_n_steps.T @ init)
 
-    matrix_n_steps = eig_vecs_right @ np.diag(eig_vals**num_steps) @ eig_vecs_left
-    return np.real(matrix_n_steps.T @ init)
+    if len(probs) > SIZE:
+        probs[10] += probs[SIZE] + probs[SIZE+1] + probs[SIZE+2]
+        probs = probs[:SIZE]
+
+    return probs
 
 def plot_matrix(matrix: np.ndarray):
     plt.figure()
@@ -101,19 +106,41 @@ def plot_board(probs: np.ndarray):
     plt.yticks([])
     plt.show()
 
+def print_field_probs(probs: np.ndarray, title: str, show_plot=False) -> None:
+    print(title)
+    print('Field\tProbability')
+    for i in range(SIZE):
+        p = probs[i]
+        print(f'{i+1}\t{p}')
+    print()
+
+    if show_plot: 
+        plot_board(probs)
+
 def main():
     dice_probs = create_dice_probs()
-    matrix = create_trans_matrix_simple(dice_probs)
-    matrix = create_trans_matrix_complex(dice_probs)
+    matrix_simple = create_trans_matrix_simple(dice_probs)
+    matrix_cplx = create_trans_matrix_complex(dice_probs, matrix_simple)
+    #plot_matrix(matrix_cplx)
 
-    probs = get_n_step_probs_power(matrix, 3)
-    print(probs)
-    probs = get_n_step_probs(matrix, 3)
-    print(probs)
+    probs = get_n_step_probs(matrix_simple, 1)
+    print_field_probs(probs, 'One Step Probs:')
 
-    #plot_matrix(matrix)
-    probs = np.arange(SIZE)
-    plot_board(probs)
+    probs = get_n_step_probs(matrix_simple, 2)
+    print_field_probs(probs, 'Two Step Probs:')
+
+    probs = get_n_step_probs(matrix_simple, 3)
+    print_field_probs(probs, 'Three Step Probs:')
+
+    probs = get_n_step_probs(matrix_simple, 1000)
+    print_field_probs(probs, 'Thousand Step Probs:')
+
+    probs = get_n_step_probs(matrix_cplx, np.inf)
+    print_field_probs(probs, 'Stationary Probs:', True)
+
+    idx_sorted = np.argsort(probs)+1
+    print('Field with max probability without start and jail:', idx_sorted[-3])
+    print('Field with min probability:', idx_sorted[0])
 
 if __name__ == '__main__':
     main()
